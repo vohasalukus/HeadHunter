a11a5ce6cccc4d06f5ab25b8e64848fc73ff8681
vohasalukus <tolkosociseti@gmail.com>
2024-10-03 21:48:32 +0500

    updated env to connect models and database

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,13 +1,25 @@
+import sys
 from logging.config import fileConfig
+from os.path import dirname, abspath
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+
+from app.database import  DATABASE_URL, Base
+from app.user.models import *
+from app.relations.relations_skill import *
+from app.relations.relations_application import *
+from app.company.models import *
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", f"{DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +30,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +77,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
